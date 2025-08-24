88b01fba3f14074798c354e61f5d8dd74a1a23cd
Cirilo Bucatcat <bucatcat1999@gmail.com>
2025-08-24 22:21:59 +0800

    layout for featured item

--- src/views/Landing/components/LandingFeatures.vue
+++ src/views/Landing/components/LandingFeatures.vue
@@ -1,31 +1,15 @@
 <template>
-    <section id="features" class="relative bg-rose-200 py-4 h-[70%]">
+    <section id="features" class="relative bg-rose-200 py-4">
         
         <h3 class="text-center text-rose-600 text-5xl my-12 font-bold font-bubblegum-sans tracking-wider">
             Featured Cakes/Bestsellers
         </h3>
 
-        <div class="flex w-full items-center justify-center gap-x-12">
-
-            <article class="w-[200px]">
-                <img :src="cake2" width="200" alt="">
-                <h3>Classic Chocolate Truffle</h3>
-                <p>Rich & moist, our customer favorite.</p>
-            </article>
-            <article class="w-[200px]">
-                <img :src="cake3" width="200" alt="">
-                <h3>Red Velvet Dream</h3>
-                <p>Smooth cream cheese frosting, a true delight.</p>
-            </article>
-            <article class="w-[200px]">
-                <img :src="cake4" width="200" alt="">
-                <h3>Ube Supreme</h3>
-                <p>A Filipino classic with a modern twist.</p>
-            </article>
-            <article class="w-[200px]">
-                <img :src="cake5" width="200" alt="">
-                <h3>Custom Celebration Cakes</h3>
-                <p>Personalized for birthdays, weddings, or any event.</p>
+        <div class="flex flex-col sm:flex-row flex-wrap  w-full items-center justify-center gap-y-12 sm:gap-x-12">
+            <article v-for="{ img, title, description } in cakes" class="grow-0 bg-slate-50 w-96 h-[450px] shrink-0 shadow rounded-md p-6 flex flex-col items-center justify-center">
+                <img :src="img" width="200" :alt="`Image for ${title}`">
+                <h3 class="text-2xl tracking-wider line-clamp-1 font-bold font-bubblegum-sans" :title>{{ title }} </h3>
+                <p class="line-clamp-3">{{ description }}</p>
             </article>
         </div>
     </section>
@@ -36,4 +20,47 @@ import cake2 from '@/assets/images/cake2.png'
 import cake3 from '@/assets/images/cake3.png'
 import cake4 from '@/assets/images/cake4.png'
 import cake5 from '@/assets/images/cake5.png'
+
+const cakes = [
+    {
+        img: cake2,
+        title: 'Classic Chocolate Truffle',
+        description: 'Rich & moist, our customer favorite.'
+    },
+    {
+        img: cake3,
+        title: 'Red Velvet Dream',
+        description: 'A Filipino classic with a modern twist.'
+    },
+    {
+        img: cake4,
+        title: 'Ube Supreme',
+        description: 'Smooth cream cheese frosting, a true delight.'
+    },
+    {
+        img: cake5,
+        title: 'Custom Celebration Cakes',
+        description: 'Personalized for birthdays, weddings, or any event.'
+    },
+    {
+        img: cake2,
+        title: 'Classic Chocolate Truffle',
+        description: 'Rich & moist, our customer favorite.'
+    },
+    {
+        img: cake3,
+        title: 'Red Velvet Dream',
+        description: 'A Filipino classic with a modern twist.'
+    },
+    {
+        img: cake4,
+        title: 'Ube Supreme',
+        description: 'Smooth cream cheese frosting, a true delight.'
+    },
+    {
+        img: cake5,
+        title: 'Custom Celebration Cakes',
+        description: 'Personalized for birthdays, weddings, or any event.'
+    },
+]
 </script>
