7931fb1112a0506d61fa3ca6cd259efc171a7e8b
Cirilo Bucatcat <bucatcat1999@gmail.com>
2025-08-24 21:59:19 +0800

    responsive for hero page

--- src/views/Landing/components/__tests__/LandingHero.test.js
+++ src/views/Landing/components/__tests__/LandingHero.test.js
@@ -1,27 +1,32 @@
 import { render, screen } from '@testing-library/vue'
 import LandingHero from '../LandingHero.vue'
+import { describe, it } from 'vitest'
 
-test('it should match the hero page texts', () => {
+// @vitest-environment happy-dom
+
+describe('component landing hero', () => {
+
+  it('should match the hero page texts', () => {
+    const { getByText } = render(LandingHero)
   
-  const { getByText } = render(LandingHero)
+    getByText('Freshly Baked Cakes')
+    getByText('Delivered to Your Doorstep')
+    getByText('Indulge in handcrafted cakes made with love, perfect for every occasion.')
+  })
 
-  getByText('Freshly Baked Cakes')
-  getByText('Delivered to Your Doorstep')
-  getByText('Indulge in handcrafted cakes made with love, perfect for every occasion.')
-
+  test('if the image element is in the page', () => {
+    
+    const { getByAltText } = render(LandingHero)
+  
+    getByAltText('Hero Master Caker')
+  })
+  
+  test('if the image element source exists', () => {
+    
+    render(LandingHero)
+  
+    const image = screen.getByAltText('Hero Master Caker')
+    expect(image).toBeTruthy();
+  })
 })
 
-test('if the image element is in the page', () => {
-  
-  const { getByAltText } = render(LandingHero)
-
-  getByAltText('Hero Master Caker')
-})
-
-test('if the image element source exists', () => {
-  
-  render(LandingHero)
-
-  const image = screen.getByAltText('Hero Master Caker')
-  expect(image).toBeTruthy();
-})
