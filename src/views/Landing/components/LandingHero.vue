caa5a437bc56aedc02fb4c93e4a1cb7ef9093d75
Cirilo Bucatcat Jr. <bucatcat1999@gmail.com>
2025-08-25 09:50:40 +0800

    fix responsive for hero page

--- src/views/Landing/components/LandingHero.vue
+++ src/views/Landing/components/LandingHero.vue
@@ -1,20 +1,20 @@
 <template>
-    <section class="relative h-[calc(100%-200px)] overflow-hidden">
+    <section class="relative h-full overflow-hidden">
         <img 
             id="img-hero" 
-            class="size-[15rem] md:size-[25rem] xl:size-[30rem] top-50 md:top-50 lg:top-50 xl:top-60 2xl:top-80  absolute-center"
+            class="size-[20rem] md:size-[25rem] 2xl:size-[30rem] top-50 md:top-50 lg:top-50 xl:top-50 2xl:top-60 absolute-center"
             :src="heroImg" 
             alt="Hero Master Caker"
         >
         <template v-for="sparklesPlacement in sparklesPlacements">
             <Sparkle :class="`${sparklesPlacement} stroke-rose-500`" />
         </template>
-        <h1 class="relative text-xl text-nowrap md:text-3xl lg:text-5xl xl:text-6xl font-bold font-bubblegum-sans text-shadow-md">
-            <span id="text-hero-1" class="text-rose-600 absolute top-30 md:top-30 lg:top-30 xl:top-30 2xl:top-50 bg-white/50 px-8 py-4 [clip-path:polygon(4%_0%,100%_1%,95%_100%,0%_100%)]">Freshly Baked Cakes</span>
-            <span id="text-hero-2" class="text-teal-600 absolute top-50 md:top-55 lg:top-55 xl:top-60 2xl:top-80 right-0 md:right-20 bg-white/50 px-8 py-4 [clip-path:polygon(4%_0%,100%_1%,95%_100%,0%_100%)]">Delivered to Your Doorstep</span>
+        <h1 class="relative text-xl text-nowrap md:text-3xl lg:text-5xl xl:text-5xl font-bold font-bubblegum-sans text-shadow-md">
+            <span id="text-hero-1" class="text-rose-600 absolute top-30 md:top-30 lg:top-30 xl:top-35 2xl:top-40 bg-white/50 px-8 py-4 [clip-path:polygon(4%_0%,100%_1%,95%_100%,0%_100%)]">Freshly Baked Cakes</span>
+            <span id="text-hero-2" class="text-teal-600 absolute top-50 md:top-55 lg:top-55 xl:top-60 2xl:top-65 right-0 md:right-20 bg-white/50 px-8 py-4 [clip-path:polygon(4%_0%,100%_1%,95%_100%,0%_100%)]">Delivered to Your Doorstep</span>
         </h1>
-        <p id="subtext-hero" class="text-teal-600/75 top-[60%] md:top-[65%] xl:top-[55%] 2xl:top-[80%]  text-center text-sm md:text-base xl:text-xl font-medium absolute-center w-[85%] md:w-none">Indulge in handcrafted cakes made with love, perfect for every occasion.</p>
-        <button data-testid="btn-cta" id="btn-cta" class="absolute-center btn-hero top-[70%] md:top-[75%] xl:top-[63%] 2xl:top-[90%] flex justify-center items-center space-x-2 xl:w-[200px] xl:h-[55px]">
+        <p id="subtext-hero" class="text-teal-600/75 top-[60%] md:top-[65%] xl:top-[70%] 2xl:top-[65%]  text-center text-sm md:text-base xl:text-xl font-medium absolute-center w-[85%] md:w-none">Indulge in handcrafted cakes made with love, perfect for every occasion.</p>
+        <button data-testid="btn-cta" id="btn-cta" class="absolute-center btn-hero top-[70%] md:top-[75%] xl:top-[80%] 2xl:top-[75%] flex justify-center items-center space-x-2 xl:w-[200px] xl:h-[55px]">
             <ShoppingCartIcon class="size-6" />
             <span>Shop Now</span>
         </button>
@@ -43,11 +43,10 @@ let timeline: GSAPTimeline;
 let mm = gsap.matchMedia();
 
 const breakpoints = [
-    { query: '(max-width: 425px)', left: '10%', right: '5%' },
+    { query: '(max-width: 425px)', left: '20%', right: '20%' },
     { query: '(width: 768px)', left: '20%', right: '15%' },
     { query: '(width: 1024px)', left: '15%', right: '15%' },
-    { query: '(width: 1440px)', left: '20%', right: '20%' },
-    { query: '(min-width: 1536px)', left: '30%', right: '30%' },
+    { query: '(width >= 1440px)', left: '25%', right: '25%' },
 ];
 
 onMounted(() => {
